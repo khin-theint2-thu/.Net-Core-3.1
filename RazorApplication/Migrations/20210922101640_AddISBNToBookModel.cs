1b57b10e9a2b28caf3eb4525ac3d60eb4d2838dc
Khin Theint <khin.doubletheint@gmail.com>
2021-10-19 19:31:04 +0630

    initialize

---
+++ RazorApplication/Migrations/20210922101640_AddISBNToBookModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorApplication.Migrations
+{
+    public partial class AddISBNToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Author",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
---
+++ RazorApplication/wwwroot/js/BookList.js
@@ -0,0 +1,56 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "author", "width": "25%" },
+            { "data": "isbn", "width": "25%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center" ><a href="/BookList/Upsert?id=${data}" class="btn btn - success text - white" style = "cursor: pointer; width: 80px; " > Edit</a >&nbsp; <a class="btn btn - danger text - white" style="cursor: pointer; width: 80px; " onclick="Delete('/api/book/?id=${data}')">Delete</a ></div>`
+                },"width":"25%"
+            }
+        ],
+        "language": {
+            "emptyTable":"no data found"
+        },
+        "width":"100%"
+    })
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        dangerMode: true,
+        buttons: true
+    }).then((willDelete)=> {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        alert(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        alert(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
